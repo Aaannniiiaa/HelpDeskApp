1a2b298fd8ce16a4238f1f18b2c01e027d0a221b
Aaannniiiaa <aniarot@vp.pl>
2026-06-08 21:00:22 +0200

    Initialize Springg Boot helpdesk project

---
+++ src/main/java/org/example/helpdesk/HelpDeskAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpDeskAppApplication.class, args);
+    }
+
+}
