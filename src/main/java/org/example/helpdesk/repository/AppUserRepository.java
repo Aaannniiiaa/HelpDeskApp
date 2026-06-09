416b986bd469b94aaf0bbe585d03fe76027d160e
Aaannniiiaa <aniarot@vp.pl>
2026-06-09 20:47:23 +0200

    Create JPA repositories for helpdesk entites

---
+++ src/main/java/org/example/helpdesk/repository/AppUserRepository.java
@@ -0,0 +1,13 @@
+package org.example.helpdesk.repository;
+
+import org.example.helpdesk.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByUsername(String username);
+
+    Optional<AppUser> findByEmail(String email);
+}
---
+++ src/main/java/org/example/helpdesk/repository/TicketRepository.java
@@ -0,0 +1,15 @@
+package org.example.helpdesk.repository;
+
+import org.example.helpdesk.entity.AppUser;
+import org.example.helpdesk.entity.Ticket;
+import org.example.helpdesk.enums.TicketStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    List<Ticket> findByUser(AppUser user);
+
+    List<Ticket> findByStatus(TicketStatus status);
+}
